57da117050ebd0aad9008955d0fd115136fc7546
Aswin-22 <dev.aswinm@gmail.com>
2024-10-11 13:08:46 +0530

    Oct24 - week 1

---
+++ 121.BestTimetoBuyandSellStock.py
@@ -0,0 +1,37 @@
+"""
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example 1:
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+"""
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        left = 0
+        right = 1
+        max = 0
+        n = len(prices)
+
+        while right < n:
+            curr_max = prices[right] - prices[left]
+            
+            if prices[left] < prices[right]:
+                max = max if max > curr_max else curr_max
+            else:
+                left = right
+            right += 1
+
+        return max
+
+# Time complexity O(n) and Space Complexity O(1)
+# RunTime 696ms and Memory 27.40MB
